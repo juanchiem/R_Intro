9eccfeb94f11a8635c29ff99b4fce80ee69ec27c
Juan <edwardsmolina@gmail.com>
2018-06-20 12:46:38 -0300

    boxplot

--- scripts/boxplot_modif.R
+++ scripts/boxplot_modif.R
@@ -1,17 +1,16 @@
 # 1.5(IQR) Rule
-
 a = c(3,12,15,16,17,18,19,34)
 # plot(density(a), ylim=c(0,1.2))
 # boxplot(a, horizontal = T, add=TRUE)
 
-boxplot(a, horizontal = T)
+boxplot(a, horizontal = TRUE)
 rug(a, col="blue",lwd=1)
-text(a, 0.5, as.character(a), cex = 0.7)
+# text(a, 0.5, as.character(a), cex = 0.7)
 
-# summary(a)
-# quantile(a)
-# quantile(a, 0.75) - quantile(a, 0.25)
-# ?IQR
+ # summary(a)
+ # quantile(a)
+ # quantile(a, 0.75) - quantile(a, 0.25)
+ # ?IQR
 # IQR(a)
 
 fivenum(a)
@@ -27,7 +26,7 @@ lim_sup = q3 + (1.5*(iqr))
 
 abline(v = c(lim_inf, lim_sup), lty =2, col= "blue")
 
-b = c(6.75,12,15,16,16,17,19,34)
+b = c(6.749,12,15,16,16,17,19,34)
 boxplot(b, horizontal = T)
 
 # 68 95 and 99.7 rule
